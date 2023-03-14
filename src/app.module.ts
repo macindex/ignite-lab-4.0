78eb4cbba32a961e2220b0561b46bda5425a71dc
macindex <machollanda@gmail.com>
2023-03-14 19:16:51 -0300

    dependency injection & prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
+import { HttpModule } from './http.module';
+import { SMTPMailService } from './mail/smtp-mail.service';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MailService } from './mail/mail.service';
 
 @Module({
-  imports: [],
+  imports: [
+    HttpModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: MailService,
+      useClass: SMTPMailService,
+    }
+  ],
 })
 export class AppModule {}
