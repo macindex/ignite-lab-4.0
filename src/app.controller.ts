17d4733b1a8aefef5de77b83f2c646470e4646f2
macindex <machollanda@gmail.com>
2023-03-15 20:50:35 -0300

    request validation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
+import { CreateNotificationBody } from './create-notification-body';
 import { PrismaService } from './prisma.service';
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { MailService } from './mail/mail.service';
 import { randomUUID } from 'crypto';
 
 @Controller('notifications')
 export class AppController {
+  CreateNotificationBody(arg0: string, arg1: string, arg2: string): any {
+    throw new Error('Method not implemented.');
+  }
   constructor(private readonly prisma: PrismaService) {}
 
   @Get()
@@ -13,7 +17,8 @@ export class AppController {
     return this.prisma.notification.findMany();
   }
   @Post()
-  async create(){
+  async create(@Body() body: CreateNotificationBody){
+    const { recipientId, content, category } = body
     await this.prisma.notification.create({
       data: {
         id: randomUUID(),
